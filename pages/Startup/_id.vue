1e9adc400d01ed6e297fc8e72d0b106a0fed79fd
Orazalinov Askhat <58249226+askesaske@users.noreply.github.com>
2021-04-13 17:48:25 +0600

    startup-page added

---
+++ pages/Startup/_id.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="startup-page">
+    <div class="startup-page__container">
+
+      <div class="startup-page__path-history path-history">
+        <div class="path-history__item">Программирование</div>
+        <div class="path-history__item">JavaScript</div>
+      </div>
+
+      <div class="startup-page__heading-box">
+
+        <h3 class="startup-page__heading">
+          Полный курс по JavaScript - с нуля до результата.
+        </h3>
+
+        <p class="startup-page__subtitle">
+          Полный курс по JavaScript - с нуля до результата. Полный курс по JavaScript - с нуля до результата. Полный
+          курс по JavaScript
+        </p>
+
+      </div>
+
+      <div class="startup-page__wrapper startup-info">
+
+        <div class="startup-info__row">
+
+          <div class="startup-info__images">
+
+            <div class="startup-info__img-box">
+              <img src="../../assets/img/startup-main-img.png" alt="" class="startup-info__main-img">
+            </div>
+
+            <div class="startup-info__img-row">
+              <img src="../../assets/img/startup-additional-img.png" alt="" class="startup-info__additional-img">
+              <img src="../../assets/img/startup-additional-img.png" alt="" class="startup-info__additional-img">
+              <img src="../../assets/img/startup-additional-img.png" alt="" class="startup-info__additional-img">
+              <img src="../../assets/img/startup-additional-img.png" alt="" class="startup-info__additional-img">
+              <img src="../../assets/img/startup-additional-img.png" alt="" class="startup-info__additional-img">
+            </div>
+
+          </div>
+
+          <div class="startup-info__details">
+
+            <div class="startup-info__progress-box progress-box">
+              <div class="progress-box__wrapper">
+                <div class="progress-box__line"></div>
+              </div>
+
+              <div class="progress-box__row">
+                <div class="progress-box__percent">
+                  71%
+                </div>
+
+                <div class="progress-box__text">
+                  Описание этапа проекта
+                  с нуля до результата
+                </div>
+
+              </div>
+            </div>
+
+            <div class="startup-info__sum">
+              <span>600 000тг</span> / 3 млн тг
+            </div>
+
+            <div class="startup-info__likes">
+              <svg width="34" height="31">
+                <use href="../../assets/img/icons.svg#heart"></use>
+              </svg>
+
+              <p>2001</p>
+            </div>
+
+            <div class="startup-info__date">
+              <p>7 апреля 2021г</p>
+              <p>Алматы</p>
+            </div>
+
+            <div class="startup-info__btn-group">
+              <button class="startup-info__btn button">
+                Связаться
+              </button>
+
+              <button class="startup-info__btn startup-info__btn--orange button">
+                Задонатить
+              </button>
+
+              <button class="startup-info__btn startup-info__btn--outline button">
+                <svg width="13" height="20">
+                  <use href="../../assets/img/icons.svg#button-favorite"></use>
+                </svg>
+
+                Сохранить
+              </button>
+            </div>
+
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
